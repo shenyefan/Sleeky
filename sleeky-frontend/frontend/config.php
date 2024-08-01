2864a532dd3d98e4d3cf851e5684962c363f5c21
Moenya <shenyefan0793@163.com>
2024-08-01 12:54:59 +0800

    Update config.php

--- sleeky-frontend/frontend/config.php
+++ sleeky-frontend/frontend/config.php
@@ -4,39 +4,39 @@
 // GENERAL
 
 // Page title for your site
-define('title', '短链生成'); 
+define('title'， '短链生成'); 
 
 // The short title of your site, used in the footer and in some sub pages
-define('shortTitle', '申业凡');
+define('shortTitle'， 'Moenya');
 
 // A description of your site, shown on the homepage.
-define('description', ''); 
+define('description'， ''); 
 
 // The favicon for your site
-define('favicon', '/frontend/assets/img/favicon.ico');
+define('favicon'， '/frontend/assets/img/favicon.ico');
 
 // Logo for your site, displayed on home page
-define('logo', '/frontend/assets/img/logo-black.png');
+define('logo'， '/frontend/assets/img/logo-black.png');
 
 // Enable reCAPTCHA V3
 // It is highly recommended you use reCAPTCHA V3. It will stop spam. You can get a site and secret key from here: https://www.google.com/recaptcha/admin/create
-define("enableRecaptcha", false);
+define("enableRecaptcha"， false);
 
 // reCAPTCHA V3 Site Key
-define("recaptchaV3SiteKey", 'YOUR_SITE_KEY_HERE');
+define("recaptchaV3SiteKey"， 'YOUR_SITE_KEY_HERE');
 
 // reCAPTCHA V3 Secret Key
-define("recaptchaV3SecretKey", 'YOUR_SECRET_KEY_HERE');
+define("recaptchaV3SecretKey"， 'YOUR_SECRET_KEY_HERE');
 
 // Enables the custom URL field
 // true or false
-define('enableCustomURL', true);
+define('enableCustomURL'， true);
 
 // Optional
 // Set a primary colour to be used. Default: #007bff
 // Here are some other colours you could try:
 // #f44336: red, #9c27b0: purple, #00bcd4: teal, #ff5722: orange
-define('colour', '#007bff');
+define('colour'， '#007bff');
 
 // Optional
 // Set a background image to be used.
